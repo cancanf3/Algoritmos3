056bc6a2a4b54425503bc12f5a2ebeea3101a814
jouvash <JouVash@MacBook-Pro-de-Jose.local>
2014-04-28 21:08:18 -0430

    Subiendo todos los codigos base del proyecto

---
+++ proyecto1/p1-codigo/Vertice.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+public class Vertice
+{
+  private String id;
+  private double peso;
+  
+  public Vertice(String id, double peso) {
+  }
+
+  public double getPeso() {
+  }
+
+  public String getId() {
+  }
+
+  public String toString() { 
+  }
+}
