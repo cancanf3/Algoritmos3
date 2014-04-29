c4a777cd487688015e87b2cab04ea38f0eff79e7
jouvash <JouVash@MacBook-Pro-de-Jose.local>
2014-04-28 22:02:04 -0430

    Completando TAD Vertice

--- proyecto1/p1-codigo/ClienteGrafo.java
+++ proyecto1/p1-codigo/ClienteGrafo.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class ClienteGrafo {
   public static void main(String [] args) {
-
+  	Vertice hola = new Vertice("123456",3);
+	System.out.println(hola.toString());
   }
 }
--- proyecto1/p1-codigo/Vertice.java
+++ proyecto1/p1-codigo/Vertice.java
@@ -7,15 +7,22 @@ public class Vertice
   private String id;
   private double peso;
   
-  public Vertice(String id, double peso) {
+  public Vertice(String id, double peso) {											// Constructor de la clase
+	this.id = id;																	// Pre: 
+  	this.peso = peso;
   }
 
   public double getPeso() {
+  return this.peso;
   }
 
   public String getId() {
+  return this.id;
   }
 
   public String toString() { 
+  	String peso = Double.toString(this.peso);
+  	String repr = "Vertice id: " + this.id + " peso: " + peso;
+  	return repr;
   }
 }
