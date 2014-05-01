d5d4e58d0d304f7647ea3e8ccd8f99383ad90e13
jouvash <JouVash@MacBook-Pro-de-Jose.local>
2014-04-30 20:45:49 -0430

    Listo los TADs: lado, arista, arco

--- proyecto1/p1-codigo/Arco.java
+++ proyecto1/p1-codigo/Arco.java
@@ -1,21 +1,28 @@
 /**
  *
  */
-
 public class Arco extends Lado
 {
   private Vertice extremoInicial;
   private Vertice extremoFinal;
   
   public Arco(String id, double peso, Vertice extremoInicial, Vertice extremoFinal) {
+  	super(id,peso);
+	this.extremoInicial = extremoInicial;
+	this.extremoFinal = extremoFinal;
   }
 
   public Vertice getExtremoInicial() {
+  	return this.extremoInicial;
   }
 
   public Vertice getExtremoFinall() {
+  	return this.extremoFinal;
   }
 
   public String toString() {
+ 	String peso = Double.toString(getPeso());
+	String repr = "Arco id: " + getId() + " peso: " + peso;
+	return repr;
   }
 }
--- proyecto1/p1-codigo/ClienteGrafo.java
+++ proyecto1/p1-codigo/ClienteGrafo.java
@@ -6,7 +6,9 @@ import java.util.*;
 
 public class ClienteGrafo {
   public static void main(String [] args) {
-  	Vertice hola = new Vertice("123456",3);
-	System.out.println(hola.toString());
+  	Vertice v1 = new Vertice("1",1);
+	Vertice v2 = new Vertice("2",2);
+  	Arco hola = new Arco("123456",3,v1,v2);
+	System.out.println(hola.getId());
   }
 }
